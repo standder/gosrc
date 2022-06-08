7e187d92d4d4532cbe5dcb527110b88427c9196f
standder <standder2@gmail.com>
2022-06-08 17:29:30 +0800

    update ex2.go

---
+++ basic/ex2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+  var a float64 = 422
+  var b float64 = 564
+  var c float64 = 255.65
+  var y bool
+  fmt.Println(float64(a+b+c))
+  fmt.Println(y)
+}
